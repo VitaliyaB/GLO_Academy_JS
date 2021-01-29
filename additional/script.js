3bfb6a251519cc365552c327186258ba587b0a4e
Vitaliya Borontsova <vborontsova@gmail.com>
2021-01-29 23:13:45 +0300

    feat: add calculations and outputs | aditional/script.js

--- additional/script.js
+++ additional/script.js
@@ -1,1 +1,18 @@
-const num = 266219;
+let num = 266219;
+let newNum = 1;
+
+num = num.toString();
+
+// multiply each digit of num
+for (let i = 0; i < num.length; i += 2) {
+  newNum *= +num[i] * +num[i+1];
+}
+
+console.log('newNum: ', newNum);
+
+// newNum raise to the power of 3
+newNum = newNum ** 3;
+console.log('newNum raise to the power of 3: ', newNum);
+
+// output first two digits of newNum
+console.log('First two digits of newNum: ', newNum.toString().substr(0, 2));
