802a142cb73921e74959f444789aef9a6cabef0c
Vitaliya Borontsova <vborontsova@gmail.com>
2021-01-29 22:10:39 +0300

    feat: declare vars | common/scripts.js

---
+++ common/script.js
@@ -0,0 +1,10 @@
+const money = 1000; // monthly income
+const income = 500; // additional income
+const addExpenses = 'phone, intetnet, taxi, bills'; // additional expenses
+const deposit = true; // have deposit?
+const mission = 10000; // amount of savings
+const period = 6; // period of savings
+
+console.log('money: ', typeof money);
+console.log('income: ', typeof income);
+console.log('deposit: ', typeof deposit);
