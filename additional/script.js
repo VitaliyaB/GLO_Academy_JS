c54e9a3f43d00df3f908b70843018eed34a1fa8e
Vitaliya Borontsova <vborontsova@gmail.com>
2021-02-03 00:18:06 +0300

    feat: add function checkString

---
+++ additional/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+let checkString = function (data) {
+  if (typeof data !== 'string') {
+    return 'Передана не строка';
+  }
+
+  data = data.trim();
+
+  if (data.length > 30) {
+    return data.substring(0, 31) + '...';
+  }
+
+  return data;
+};
+
+console.log(checkString('Lorem ipsum, dolor sit amet consectetur adipisicing'));
+console.log(checkString('    Lorem ipsum, dolor sit amet consectetur adipisicing '));
+console.log(checkString('Lorem ipsum, dolor sit'));
+console.log(checkString('Lorem ipsum, dolor sit   '));
+console.log(checkString(123));
+console.log(checkString(true));
+
