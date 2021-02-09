d38e24d74c9f010ed53dacbf8b0bbf8b1d467083
Vitaliya Borontsova <vborontsova@gmail.com>
2021-02-09 22:32:52 +0300

    feat: find all DOM elements

--- common/script.js
+++ common/script.js
@@ -1,5 +1,41 @@
 'use strict';
 
+let incomeAddBtn, expensesAddBtn;
+let startBtn = document.getElementById('start');
+let buttonTag = document.getElementsByTagName('button');
+
+for (let i = 0; i < buttonTag.length; i++) {
+  if (buttonTag[i].classList.contains('income_add')) {
+    incomeAddBtn = buttonTag[i];
+    continue;
+  }
+
+  if (buttonTag[i].classList.contains('expenses_add')) {
+    expensesAddBtn = buttonTag[i];
+    continue;
+  }
+}
+
+let depositCheckmark = document.querySelector('#deposit-check');
+let additionalInput = document.querySelectorAll('.additional_income-item');
+
+let budgetMonthResult = document.getElementsByClassName('budget_month-value');
+let budgetDayResult = document.getElementsByClassName('budget_day-value');
+let expensesMonthResult = document.getElementsByClassName('expenses_month-value');
+let additionalIncomeResult = document.getElementsByClassName('additional_income-value');
+let additionalExpensesResult = document.getElementsByClassName('additional_expenses-value');
+let incomePeriodResult = document.getElementsByClassName('income_period-value');
+let targetMonthResult = document.getElementsByClassName('target_month-value');
+
+let salaryAmountInput = document.querySelector('.salary-amount');
+let incomeTitleInput = document.querySelector('.income-items .income-title');
+let incomeAmountInput = document.querySelector('.income-amount');
+let expensesTitleInput = document.querySelector('.expenses-items .expenses-title');
+let expensesAmountInput = document.querySelector('.expenses-amount');
+let additionalExpensesItemInput = document.querySelector('.additional_expenses-item');
+let targetAmountInput = document.querySelector('.target-amount');
+let periodSelectRange = document.querySelector('.period-select');
+
 let money,
   // monthly income
   start = function () {
@@ -101,3 +137,24 @@ for (let key in appData) {
 function isNumber(data) {
   return !isNaN(parseFloat(data)) && isFinite(parseFloat(data));
 }
+
+console.log('startBtn: ', startBtn);
+console.log('incomeAddBtn: ', incomeAddBtn);
+console.log('expensesAddBtn: ', expensesAddBtn);
+console.log('depositCheckmark: ', depositCheckmark);
+console.log('additionalInput: ', additionalInput);
+console.log('budgetMonthResult: ', budgetMonthResult);
+console.log('budgetDayResult: ', budgetDayResult);
+console.log('expensesMonthResult: ', expensesMonthResult);
+console.log('additionalIncomeResult: ', additionalIncomeResult);
+console.log('additionalExpensesResult: ', additionalExpensesResult);
+console.log('incomePeriodResult: ', incomePeriodResult);
+console.log('targetMonthResult: ', targetMonthResult);
+console.log('salaryAmountInput: ', salaryAmountInput);
+console.log('incomeTitleInput: ', incomeTitleInput);
+console.log('incomeAmountInput: ', incomeAmountInput);
+console.log('expensesTitleInput: ', expensesTitleInput);
+console.log('expensesAmountInput: ', expensesAmountInput);
+console.log('additionalExpensesItemInput: ', additionalExpensesItemInput);
+console.log('targetAmountInput: ', targetAmountInput);
+console.log('periodSelectRange: ', periodSelectRange);
