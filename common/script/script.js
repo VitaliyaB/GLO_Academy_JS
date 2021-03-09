e7f58da682ad7db8d977a25a038055572d6728e1
Vitaliya Borontsova <vborontsova@gmail.com>
2021-03-10 00:53:16 +0300

    feat: add ajax request

--- common/script/script.js
+++ common/script/script.js
@@ -434,6 +434,57 @@ window.addEventListener('DOMContentLoaded', () => {
 
   };
 
+  // * send-ajax-form
+  const sendForm = () => {
+    const errorMessage = 'Что-то пошло не так...';
+    const loadMessage = 'Загрузка...';
+    const successMessage = 'Спасибо! Мы скоро с вами свяжемся!';
+    const form = document.getElementById('form1');
+    const statusMessage = document.createElement('div');
+
+    statusMessage.style.cssText = 'font-size: 2rem;';
+
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+      form.appendChild(statusMessage);
+      statusMessage.textContent = loadMessage;
+
+      const formData = new FormData(form);
+      const body = {};
+
+      for (const val of formData.entries()) {
+        body[val[0]] = val[1];
+      }
+
+      postData(body, () => {
+        statusMessage.textContent = successMessage;
+      }, (error) => {
+        console.error(error);
+        statusMessage.textContent = errorMessage;
+      });
+    });
+
+    const postData = (body, outputData, errorData) => {
+      const request = new XMLHttpRequest();
+
+      request.addEventListener('readystatechange', () => {
+        if (request.readyState !== 4) {
+          return;
+        }
+
+        if (request.status === 200) {
+          outputData();
+        } else {
+          errorData(request.status);
+        }
+      });
+
+      request.open('POST', './server.php');
+      request.setRequestHeader('Content-Type', 'application/json');
+      request.send(JSON.stringify(body));
+    };
+  };
+
   countTimer('10 march 2021');
   toggleMenu();
   togglePopUp();
@@ -442,4 +493,5 @@ window.addEventListener('DOMContentLoaded', () => {
   command();
   validate();
   calc(100);
+  sendForm();
 });
