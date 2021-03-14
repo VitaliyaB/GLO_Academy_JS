85b3e0376ce4a12289422130fec19354fb63169d
Vitaliya Borontsova <vborontsova@gmail.com>
2021-03-15 00:39:40 +0300

    feat: add animation for calc

--- additional/src/modules/calc.js
+++ additional/src/modules/calc.js
@@ -5,6 +5,9 @@ const calc = (price = 100) => {
   const calcCount = document.querySelector('.calc-count');
   const calcDay = document.querySelector('.calc-day');
   const totalValue = document.getElementById('total');
+  let countSumTotal = 0;
+  let step = 1;
+  const animationTime = 2000;
 
   const countSum = () => {
     let total = 0;
@@ -27,7 +30,32 @@ const calc = (price = 100) => {
       total = price * typeValue * squareValue * countValue * dayValue;
     }
 
-    totalValue.textContent = total;
+    if (total < 500) {
+      step = 10;
+    } else if (total > 10000) {
+      step = 1000;
+    } else {
+      step = 100;
+    }
+
+    const time = Math.round(animationTime / (total / step));
+    countSumTotal = total % step;
+
+    const setTotal = () => {
+      if (total > 0) {
+        countSumTotal += step;
+        totalValue.textContent = countSumTotal;
+
+        if (countSumTotal >= total) {
+          countSumTotal = 0;
+          clearInterval(timerId);
+        }
+      } else {
+        clearInterval(timerId);
+      }
+    };
+
+    const timerId = setInterval(setTotal, time);
   };
 
   calcBlock.addEventListener('change', (event) => {
