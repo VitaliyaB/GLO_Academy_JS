7a5475345bd8fe1cf8a8b69ffdf6dcf33460cf41
Vitaliya Borontsova <vborontsova@gmail.com>
2021-03-20 01:29:57 +0300

    feat: change ajax to promise

--- additional/script.js
+++ additional/script.js
@@ -1,28 +1,45 @@
+'use strict';
+
 document.addEventListener('DOMContentLoaded', () => {
-    'use strict';
 
-    const select = document.getElementById('cars'),
-        output = document.getElementById('output');
+  const select = document.getElementById('cars');
+  const output = document.getElementById('output');
 
-    select.addEventListener('change', () => {
-        const request = new XMLHttpRequest();
-        request.open('GET', './cars.json');
-        request.setRequestHeader('Content-type', 'application/json');
-        request.send();
-        request.addEventListener('readystatechange', () => {
-            if (request.readyState === 4 && request.status === 200) {
-                const data = JSON.parse(request.responseText);
-                data.cars.forEach(item => {
-                    if (item.brand === select.value) {
-                        const {brand, model, price} = item;
-                        output.innerHTML = `Тачка ${brand} ${model} <br>
-                        Цена: ${price}$`;
-                    }
-                });
-            } else {
-                output.innerHTML = 'Произошла ошибка';
-            }
-        });
+  const getData = () => new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+
+    request.addEventListener('readystatechange', () => {
+      if (request.readyState !== 4) {
+        return;
+      }
+
+      if (request.status === 200) {
+        const data = JSON.parse(request.responseText);
+        resolve(data);
+      } else {
+        reject(request.status);
+      }
     });
 
+    request.open('GET', 'cars.json');
+    request.setRequestHeader('Content-type', 'application/json');
+    request.send();
+  });
+
+  select.addEventListener('change', () => {
+    getData()
+      .then((response) => {
+        response.cars.forEach((item) => {
+          if (item.brand === select.value) {
+            const { brand, model, price } = item;
+            output.innerHTML = `Тачка ${brand} ${model} <br>
+                        Цена: ${price}$`;
+          }
+        });
+      })
+      .catch((err) => {
+        console.log('select.addEventListener ~ err', err);
+        output.innerHTML = 'Произошла ошибка';
+      });
+  });
 });
