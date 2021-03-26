80ebb690010da8b318d39d8db4ab37740b93da3e
Vitaliya Borontsova <vborontsova@gmail.com>
2021-03-27 01:45:47 +0300

    feat: add popup hint

--- src/modules/showHint.js
+++ src/modules/showHint.js
@@ -5,13 +5,23 @@ const showHint = () => {
     const target = event.target;
     const eventFormula = target.closest('.formula-item');
     const popUpItem = eventFormula.querySelector('.formula-item-popup');
+    const itemDescr = eventFormula.querySelector('.formula-item__descr');
 
     popUpItem.style.visibility = 'visible';
+    eventFormula.classList.add('active-item');
+
+    const hintTop = popUpItem.getBoundingClientRect().top;
+
+    if (hintTop < 0) {
+      eventFormula.classList.add('active-item__reverse');
+      itemDescr.style.visibility = 'hidden';
+    }
 
     // eventFormula.addEventListener('mouseout', () => {
+    //   eventFormula.classList.remove('active-item');
     //   popUpItem.style.visibility = 'hidden';
+    //   itemDescr.style.visibility = 'visible';
     // });
-
   };
 
   formulaItem.forEach((item) => {
